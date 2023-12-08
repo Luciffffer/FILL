9105aa2e172dc227026a2faea8b7f6abef14eef6
Lucifer <tim.noelmans@outlook.com>
2023-12-08 15:30:06 +0100

    Hello World

--- src/App.java
+++ src/App.java
@@ -3,5 +3,6 @@ import FILL.models.core.User;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
