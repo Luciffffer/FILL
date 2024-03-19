12827ead573e6be54b78dd4a90672759733e9da8
Lucifer <tim.noelmans@outlook.com>
2024-03-19 20:57:19 +0100

    chore(gameview): cleanup packages

--- src/main/java/be/kdg/fill/views/game/GameView.java
+++ src/main/java/be/kdg/fill/views/game/GameView.java
@@ -23,7 +23,6 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Rotate;
 
